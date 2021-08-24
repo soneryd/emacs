1f75853f262f978cdcb34fab4b7864c2fd76db38
soneryd <max.soneryd@gmail.com>
2021-08-24 20:09:25 +0200

    Automatically generated

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(doom-modeline counsel swiper ivy which-key w3m vterm use-package-hydra sudoku solarized-theme slime-company sclang-extensions perspective paredit org-noter-pdftools nov mentor magit lsp-ui lsp-java helm-projectile helm-fuzzy-find flycheck eyebrowse emms-state emms-mode-line-cycle delight dashboard dash-functional company-lsp chess ccls ace-jump-mode ace-jump-helm-line ac-slime ac-cider)))
+   '(emms-player-mplayer emms-setup ivy-posframe doom-themes helpful ivy-rich rainbow-delimiters doom-modeline counsel swiper ivy which-key w3m vterm use-package-hydra sudoku solarized-theme slime-company sclang-extensions perspective paredit org-noter-pdftools nov mentor magit lsp-ui lsp-java helm-projectile helm-fuzzy-find flycheck eyebrowse emms-state emms-mode-line-cycle delight dashboard dash-functional company-lsp chess ccls ace-jump-mode ace-jump-helm-line ac-slime ac-cider)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

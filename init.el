9d1f0ffbaf9d0cebd4a96062cb4cacb10c369fd5
soneryd <max.soneryd@gmail.com>
2021-08-24 11:24:34 +0200

    Added basic gui settings

---
+++ init.el
@@ -0,0 +1,16 @@
+(org-babel-load-file
+ (expand-file-name "settings.org"
+                   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(doom-modeline counsel swiper ivy which-key w3m vterm use-package-hydra sudoku solarized-theme slime-company sclang-extensions perspective paredit org-noter-pdftools nov mentor magit lsp-ui lsp-java helm-projectile helm-fuzzy-find flycheck eyebrowse emms-state emms-mode-line-cycle delight dashboard dash-functional company-lsp chess ccls ace-jump-mode ace-jump-helm-line ac-slime ac-cider)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

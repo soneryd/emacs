c6e6f61ddca462fee9f404bdaa41b0af86daf761
soneryd <max.soneryd@gmail.com>
2021-12-02 22:40:56 +0100

    big big commit

--- init.el
+++ init.el
@@ -6,8 +6,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(notmuch-search-oldest-first nil)
+ '(org-babel-load-languages '((C . t) (emacs-lisp . t)))
  '(package-selected-packages
-   '(emms-player-mplayer emms-setup ivy-posframe doom-themes helpful ivy-rich rainbow-delimiters doom-modeline counsel swiper ivy which-key w3m vterm use-package-hydra sudoku solarized-theme slime-company sclang-extensions perspective paredit org-noter-pdftools nov mentor magit lsp-ui lsp-java helm-projectile helm-fuzzy-find flycheck eyebrowse emms-state emms-mode-line-cycle delight dashboard dash-functional company-lsp chess ccls ace-jump-mode ace-jump-helm-line ac-slime ac-cider)))
+   '(csharp-mode notmuch lsp-mode lsp helm slime company org-pdftools org-pdftool erc-sasl emms-player-mplayer emms-setup ivy-posframe doom-themes helpful ivy-rich rainbow-delimiters doom-modeline counsel swiper ivy which-key w3m vterm use-package-hydra sudoku solarized-theme slime-company sclang-extensions perspective paredit org-noter-pdftools nov mentor magit lsp-ui lsp-java helm-projectile helm-fuzzy-find flycheck eyebrowse emms-state emms-mode-line-cycle delight dashboard dash-functional company-lsp chess ccls ace-jump-mode ace-jump-helm-line ac-slime ac-cider)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
